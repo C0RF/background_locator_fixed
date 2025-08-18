d39e3fac588c8f0b948709d84b556a9ce196f7da
mostfamhmd <mmost1238@gmail.com>
2025-08-18 19:19:20 +0300

    push again

--- android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
@@ -1,65 +1,53 @@
 package yukams.app.background_locator_2.provider
 
 import android.location.Location
-import android.os.Build
 import com.google.android.gms.location.LocationResult
-import yukams.app.background_locator_2.Keys
 
-class LocationParserUtil {
-    companion object {
+object LocationParserUtil {
 
-        fun getLocationMapFromLocation(location: Location): HashMap<Any, Any?> {
-            var speedAccuracy = 0f
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                speedAccuracy = location.speedAccuracyMetersPerSecond
-            }
-            var isMocked = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                isMocked = location.isFromMockProvider
-            }
-
-            val map = hashMapOf<String, Any?>(
-                Keys.ARG_IS_MOCKED to isMocked,
-                Keys.ARG_LATITUDE to location.latitude,
-                Keys.ARG_LONGITUDE to location.longitude,
-                Keys.ARG_ACCURACY to location.accuracy,
-                Keys.ARG_ALTITUDE to location.altitude,
-                Keys.ARG_SPEED to location.speed,
-                Keys.ARG_SPEED_ACCURACY to speedAccuracy,
-                Keys.ARG_HEADING to location.bearing,
-                Keys.ARG_TIME to location.time.toDouble(),
-                Keys.ARG_PROVIDER to location.provider,
-            )
-
-            // نعمل cast هنا عشان الكومبايلر يرضى
-            return map as HashMap<Any, Any?>
+    fun getLocationMapFromLocation(location: Location): HashMap<Any, Any> {
+        val map = HashMap<Any, Any>()
+        map["latitude"] = location.latitude
+        map["longitude"] = location.longitude
+        map["accuracy"] = location.accuracy
+        map["altitude"] = location.altitude
+        map["bearing"] = location.bearing
+        map["speed"] = location.speed
+        map["time"] = location.time
+        map["provider"] = location.provider
+        map["isMocked"] = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            location.isFromMockProvider
+        } else {
+            false
         }
-
-        fun getLocationMapFromLocation(location: LocationResult?): HashMap<Any, Any?>? {
-            val firstLocation = location?.lastLocation ?: return null
-
-            var speedAccuracy = 0f
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                speedAccuracy = firstLocation.speedAccuracyMetersPerSecond
-            }
-            var isMocked = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                isMocked = firstLocation.isFromMockProvider
-            }
-
-            val map = hashMapOf<String, Any?>(
-                Keys.ARG_IS_MOCKED to isMocked,
-                Keys.ARG_LATITUDE to firstLocation.latitude,
-                Keys.ARG_LONGITUDE to firstLocation.longitude,
-                Keys.ARG_ACCURACY to firstLocation.accuracy,
-                Keys.ARG_ALTITUDE to firstLocation.altitude,
-                Keys.ARG_SPEED to firstLocation.speed,
-                Keys.ARG_SPEED_ACCURACY to speedAccuracy,
-                Keys.ARG_HEADING to firstLocation.bearing,
-                Keys.ARG_TIME to firstLocation.time.toDouble()
-            )
-
-            return map as HashMap<Any, Any?>
+        map["speedAccuracy"] = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            location.speedAccuracyMetersPerSecond
+        } else {
+            0f
         }
+        return map
+    }
+
+    fun getLocationMapFromLocation(locationResult: LocationResult?): HashMap<Any, Any>? {
+        val location = locationResult?.lastLocation ?: return null
+        val map = HashMap<Any, Any>()
+        map["latitude"] = location.latitude
+        map["longitude"] = location.longitude
+        map["accuracy"] = location.accuracy
+        map["altitude"] = location.altitude
+        map["bearing"] = location.bearing
+        map["speed"] = location.speed
+        map["time"] = location.time
+        map["isMocked"] = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            location.isFromMockProvider
+        } else {
+            false
+        }
+        map["speedAccuracy"] = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            location.speedAccuracyMetersPerSecond
+        } else {
+            0f
+        }
+        return map
     }
 }
